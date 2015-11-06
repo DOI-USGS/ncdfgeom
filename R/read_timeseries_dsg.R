d0ebd50fcbb478734c815118c8a81d1286b6cc83
David Blodgett <dblodgett@usgs.gov>
2015-11-05 19:14:57 -0700

    need to use variable not dimension

--- R/read_timeseries_dsg.R
+++ R/read_timeseries_dsg.R
@@ -111,7 +111,7 @@ read_timeseries_dsg = function(nc_file){
 		nc_list$varmeta[data_var][[1]]$name<-nc$var[data_var][[1]]$name
 		nc_list$varmeta[data_var][[1]]$long_name<-nc$var[data_var][[1]]$longname
 		nc_list$data_frames[data_var][[1]]<-as.data.frame(get.var.ncdf(nc,data_var,c(1,1),c(-1,-1)))
-		colnames(nc_list$data_frames[data_var][[1]])<-as.character(nc$dim$station$vals)
+		colnames(nc_list$data_frames[data_var][[1]])<-as.character(get.var.ncdf(nc,timeseries_id))
 	}
 	
 	nc_list$global_attributes$nc_summary<-att.get.ncdf(nc,0,'summary')$value
