ffad6dfd83424b84764a1e9775bcac68db435003
David Blodgett <dblodgett@usgs.gov>
2015-12-16 16:32:35 -0800

    ncdf4 refactor

--- R/write_timeseries_dsg.R
+++ R/write_timeseries_dsg.R
@@ -30,7 +30,7 @@
 #'@references
 #'http://www.unidata.ucar.edu/software/thredds/current/netcdf-java/reference/FeatureDatasets/CFpointImplement.html
 #'
-#'@import ncdf
+#'@importFrom ncdf4 nc_create nc_close ncvar_def ncvar_put ncatt_put ncdim_def
 #'@importFrom methods is
 #'
 #'@export
@@ -64,71 +64,73 @@ write_timeseries_dsg = function(nc_file, station_names, lats, lons, times, data,
 	
 	
 	#Lay the foundation. This is a point featureType. Which has one dimension, "obs"
-	#obs_dim = dim.def.ncdf('obs', '', 1:n, unlim = TRUE, create_dimvar=FALSE)
-	station_dim = dim.def.ncdf('station', '', 1:n, create_dimvar=FALSE)
-	time_dim = dim.def.ncdf('time', '', 1:nt, unlim=FALSE, create_dimvar=FALSE)
-	strlen_dim = dim.def.ncdf('name_strlen', '', 1:max(sapply(station_names, nchar)), create_dimvar=FALSE)
+	#obs_dim = ncdim_def('obs', '', 1:n, unlim = TRUE, create_dimvar=FALSE)
+	station_dim = ncdim_def('station', '', 1:n, create_dimvar=FALSE)
+	time_dim = ncdim_def('time', '', 1:nt, unlim=FALSE, create_dimvar=FALSE)
+	strlen_dim = ncdim_def('name_strlen', '', 1:max(sapply(station_names, nchar)), create_dimvar=FALSE)
 	
 	#Setup our spatial and time info
-	station_var = var.def.ncdf('station_name', '', list(strlen_dim, station_dim), missval='', prec='char', longname='Station Names')
-	time_var 		= var.def.ncdf('time','days since 1970-01-01 00:00:00', time_dim, -999, prec='double', longname='time of measurement')
-	lat_var 		= var.def.ncdf('lat', 'degrees_north', station_dim, -999, prec='double', longname = 'latitude of the observation')
-	lon_var 		= var.def.ncdf('lon', 'degrees_east', station_dim, -999, prec='double', longname = 'longitude of the observation')
+	station_var = ncvar_def('station_name', '', dim=list(strlen_dim, station_dim), missval=NULL, prec='char', longname='Station Names')
+	time_var 		= ncvar_def('time','days since 1970-01-01 00:00:00', dim=time_dim, -999, prec='double', longname='time of measurement')
+	lat_var 		= ncvar_def('lat', 'degrees_north', dim=station_dim, -999, prec='double', longname = 'latitude of the observation')
+	lon_var 		= ncvar_def('lon', 'degrees_east', dim=station_dim, -999, prec='double', longname = 'longitude of the observation')
 
 	if(!is.na(alts[1])){
-		alt_var = var.def.ncdf('alt', 'm', station_dim, -999, prec='double', longname='vertical distance above the surface')
+		alt_var = ncvar_def('alt', 'm', dim=station_dim, missval=-999, prec='double', longname='vertical distance above the surface')
 	}
 	data_vars = list()
 	data_name = data_metadata[['name']]
-	data_vars[[1]] = var.def.ncdf(data_name, data_unit, list(time_dim, station_dim), prec=data_prec, 
+	data_vars[[1]] = ncvar_def(data_name, data_unit, dim=list(time_dim, station_dim), prec=data_prec, 
 																longname=data_metadata[['long_name']], missval=-999)
 	if(!is.na(alts[1])){
-		nc_file = create.ncdf(nc_file, vars = c(list(lat_var, lon_var, time_var, alt_var, station_var), data_vars))
+		nc = nc_create(nc_file, vars = c(list(lat_var, lon_var, time_var, alt_var, station_var), data_vars))
 	} else {
-		nc_file = create.ncdf(nc_file, vars = c(list(lat_var, lon_var, time_var, station_var), data_vars))
+		nc = nc_create(nc_file, vars = c(list(lat_var, lon_var, time_var, station_var), data_vars))
 	}
+	nc_close(nc)
+	nc<-nc_open(nc_file, write = TRUE)
 	#add standard_names
-	att.put.ncdf(nc_file, 'lat', 'standard_name', 'latitude')
-	att.put.ncdf(nc_file, 'time', 'standard_name', 'time')
-	att.put.ncdf(nc_file, 'lon', 'standard_name', 'longitude')
+	ncatt_put(nc, 'lat', 'standard_name', 'latitude')
+	ncatt_put(nc, 'time', 'standard_name', 'time')
+	ncatt_put(nc, 'lon', 'standard_name', 'longitude')
 	
 	if(!is.na(alts[1])){
-		att.put.ncdf(nc_file, 'alt', 'standard_name', 'height')
+		ncatt_put(nc, 'alt', 'standard_name', 'height')
 	}
 	
-	att.put.ncdf(nc_file, 'station_name', 'cf_role', 'timeseries_id')
-	att.put.ncdf(nc_file, 'station_name','standard_name','station_id')
+	ncatt_put(nc, 'station_name', 'cf_role', 'timeseries_id')
+	ncatt_put(nc, 'station_name','standard_name','station_id')
 	
 	#Add coordinates
 	if(!is.na(alts[1])){
-		att.put.ncdf(nc_file, data_name, 'coordinates', 'time lat lon alt')
+		ncatt_put(nc, data_name, 'coordinates', 'time lat lon alt')
 	} else {
-		att.put.ncdf(nc_file, data_name, 'coordinates', 'time lat lon')
+		ncatt_put(nc, data_name, 'coordinates', 'time lat lon')
 	}
 	
 	#Important Global Variables
-	att.put.ncdf(nc_file, 0,'Conventions','CF-1.7')
-	att.put.ncdf(nc_file, 0,'featureType','timeSeries')
-	att.put.ncdf(nc_file, 0,'cdm_data_type','Station')
-	att.put.ncdf(nc_file, 0,'standard_name_vocabulary','CF-1.7')
+	ncatt_put(nc, 0,'Conventions','CF-1.7')
+	ncatt_put(nc, 0,'featureType','timeSeries')
+	ncatt_put(nc, 0,'cdm_data_type','Station')
+	ncatt_put(nc, 0,'standard_name_vocabulary','CF-1.7')
 	
 	#Put data in NC file
-	put.var.ncdf(nc_file, time_var, as.numeric(times)/86400, count=nt) #convert to days since 1970-01-01
-	put.var.ncdf(nc_file, lat_var, lats, count=n)
-	put.var.ncdf(nc_file, lon_var, lons, count=n)
+	ncvar_put(nc, time_var, as.numeric(times)/86400, count=nt) #convert to days since 1970-01-01
+	ncvar_put(nc, lat_var, lats, count=n)
+	ncvar_put(nc, lon_var, lons, count=n)
 	
 	if(!is.na(alts[1])){
-		put.var.ncdf(nc_file, alt_var, alts, count=n)
+		ncvar_put(nc, alt_var, alts, count=n)
 	}
-	put.var.ncdf(nc_file, station_var, station_names, count=c(-1,n))
-	put.var.ncdf(nc_file, data_name, as.matrix(data), start=c(1,1), count=c(nt, n))
+	ncvar_put(nc, station_var, station_names, count=c(-1,n))
+	ncvar_put(nc, data_name, as.matrix(data), start=c(1,1), count=c(nt, n))
 	
 	#Add the optional global attributes
 	if(length(attributes)>0){
 		for(i in 1:length(attributes)){
-			att.put.ncdf(nc_file, 0, names(attributes)[i], attributes[[i]])
+			ncatt_put(nc, 0, names(attributes)[i], attributes[[i]])
 		}
 	}
 	
-	close.ncdf(nc_file)
+	nc_close(nc)
 }
