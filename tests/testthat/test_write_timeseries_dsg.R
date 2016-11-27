ff2650ddb571c5d87794ecabfe581a00f390ddfa
David Blodgett <dblodgett@usgs.gov>
2016-11-27 14:38:59 -0600

    initial test for soilmoisturetools, needs some expectations.

--- tests/testthat/test_write_timeseries_dsg.R
+++ tests/testthat/test_write_timeseries_dsg.R
@@ -1,4 +1,6 @@
 context("Create Station Timeseries DSG File")
+library(soilmoisturetools)
+library(ncdf4)
 
 test_that("Create basic DSG file",{
 	nc_summary<-'test summary'
@@ -9,8 +11,8 @@ test_that("Create basic DSG file",{
 	nc_proc_level='just a test no processing'
 	nc_title<-'test title'
 	global_attributes<-list(title = nc_title, summary = nc_summary, date_created=nc_date_create, 
-									 creator_name=nc_creator_name,creator_email=nc_creator_email,
-									 project=nc_project, processing_level=nc_proc_level)
+		creator_name=nc_creator_name,creator_email=nc_creator_email,
+		project=nc_project, processing_level=nc_proc_level)
 	
 	nc_file<-'data/test_output.nc'
 	
@@ -37,4 +39,39 @@ test_that("Create basic DSG file",{
 	expect_equivalent(ncvar_get(testnc,varid="BCCA_0-125deg_pr_day_ACCESS1-0_rcp45_r1i1p1")[,71],data$`71`)
 	expect_equivalent(testnc$var$`BCCA_0-125deg_pr_day_ACCESS1-0_rcp45_r1i1p1`$units,"mm/d")
 	expect_equivalent(ncatt_get(testnc,varid=0,"summary")$value,'test summary')
+})
+
+test_that('soilmoisturetools data writes as expected', {
+	ok = ok_data()
+	ok_meta = ok_sites_metadata(ok$station)
+	attributes <- list(
+		title = 'National Soil Moisture Network SOS',
+		abstract = 'This service provides soil moisture data from the U.S.
+		National Soil Moisture Network Pilot and serves data from SCAN, CRN,
+		West Texas and Oklahoma Mesonets. This SOS web service delivers the data
+		using GML.',
+		'provider name' = 'U.S. Geological Survey, Office of Water Information,
+		Center for Integrated Data Analytics, United States Government',
+		'provider site' = 'http://cida.usgs.gov',
+		description = 'Percentile of Volumetric Soil Moisture as compared
+		to the historical distribution. Percentiles are calculated using
+		cumulative distribution functions and range from 0-100.'
+	)
+	nc_file <- write_timeseries_dsg(
+		nc_file = tempfile(),
+		station_names = ok$station,
+		lats = ok_meta$latitude,
+		lons = ok_meta$longitude,
+		alts = ok_meta$elevation,
+		times = ok$datetime[1],
+		data = as.data.frame(array(ok$value, dim = c(
+			1, length(ok$value)
+		))),
+		data_unit = "percent",
+		data_prec = "double",
+		attributes = attributes
+	)
+	
+	nc <- nc_open(nc_file)
+	
 })
