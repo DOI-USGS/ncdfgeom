417d27952084e8f9b5a0797e9620a29f982150e4
David Blodgett <dblodgett@usgs.gov>
2017-02-24 09:51:55 -0600

    Starting to sort out the point dsg data type.

--- R/write_point_dsg.R
+++ R/write_point_dsg.R
@@ -21,29 +21,40 @@
 #'
 #'@export
 write_point_dsg = function(nc_file, lats, lons, alts, data, data_units=rep('', ncol(data)),
-													 data_prec=rep('double', ncol(data))){
+													 data_prec=rep('double', ncol(data)), feature_names = NULL){
 	
 	n = length(lats)
 	if(length(lats)!=n || length(lons)!=n || length(alts)!=n){
 		stop('lats, lons, and alts must all be vectors of the same length')
 	}
 	
-	
 	#Lay the foundation. This is a point featureType. Which has one dimension, "obs"
 	obs_dim = ncdim_def('obs', '', 1:n, create_dimvar=FALSE)
+	
+	vars_to_write <- list()
+	
+	if(!is.null(feature_names)) {
+		if(length(lats)!=n || length(lons)!=n){
+			stop('feature_names, lats, and lons must all be vectors of the same length')
+		}
+		strlen_dim = ncdim_def('name_strlen', '', 1:max(sapply(feature_names, nchar)), create_dimvar=FALSE)
+		feature_var = ncvar_def('feature_name', '', dim=list(strlen_dim, obs_dim), missval=NULL, prec='char', longname='Feature Names')
+		vars_to_write <- c(vars_to_write, list(feature_var))
+	}
 	
 	#Setup our spatial and info
 	lat_var = ncvar_def('lat', 'degrees_north', obs_dim, -999, prec='double', longname = 'latitude of the observation')
 	lon_var = ncvar_def('lon', 'degrees_east', obs_dim, -999, prec='double', longname = 'longitude of the observation')
 	alt_var = ncvar_def('alt', 'm', obs_dim, -999, prec='double', longname='vertical distance above the surface')
+	vars_to_write <- c(vars_to_write, list(lat_var, lon_var, alt_var))
 	
 	data_names = names(data)
 	data_vars  = list()
 	for(i in 1:ncol(data)){
-		data_vars[[i]] = ncvar_def(data_names[i], data_units[i], obs_dim, prec=data_prec[i], missval=-999)
+		vars_to_write <- c(vars_to_write, list(ncvar_def(data_names[i], data_units[i], obs_dim, prec=data_prec[i], missval=-999)))
 	}
 	
-	nc = nc_create(nc_file, vars = c(list(lat_var, lon_var, alt_var), data_vars))
+	nc = nc_create(nc_file, vars = vars_to_write)
 	
 	#add standard_names
 	ncatt_put(nc, 'lat', 'standard_name', 'latitude')
@@ -52,13 +63,19 @@ write_point_dsg = function(nc_file, lats, lons, alts, data, data_units=rep('', n
 	
 	#use the same names for "standard names" and add coordinates as well
 	for(i in 1:ncol(data)){
-		ncatt_put(nc, data_names[i], 'standard_name', data_names[i])
 		ncatt_put(nc, data_names[i], 'coordinates', 'lat lon alt')
 	}
 	#some final stuff
 	ncatt_put(nc, 0,'featureType','point')
+	ncatt_put(nc, 0,'Conventions','CF-1.7')
+	ncatt_put(nc, 0,'cdm_data_type','Station')
 	
 	#Put data in NC file
+		if(!is.null(feature_names)) {
+		ncatt_put(nc, 'feature_name', 'cf_role', 'timeseries_id')
+		ncvar_put(nc, 'feature_name', feature_names)
+	}
+	
 	ncvar_put(nc, 'lat', lats)
 	ncvar_put(nc, 'lon', lons)
 	ncvar_put(nc, 'alt', alts)
---
+++ tests/testthat/test_write_point_data.R
@@ -0,0 +1,20 @@
+library("ncdf4")
+
+context("Test creating a netcdf file with instance data content.")
+
+test_that("A dataframe can be round tripped to netCDF.", {
+  dataFrame <- read.csv("data/yahara_alb_attributes.csv")
+  nc_file <- write_point_dsg(nc_file=tempfile(), lats = dataFrame$YCOORD, lons = dataFrame$XCOORD, 
+  													 alts = rep(0, length(dataFrame$XCOORD)),
+  													 feature_names = dataFrame$ID,
+  													 data = dataFrame[c("GRIDCODE", "X_COORD", "Y_COORD")])
+  nc <- nc_open(nc_file)
+  expect_equal(class(nc),"ncdf4")
+  expect_equal(as.character(ncvar_get(nc, nc$var$feature_name)), as.character(dataFrame$ID))
+  expect_equal(as.character(ncvar_get(nc, nc$var$GRIDCODE)), as.character(dataFrame$GRIDCODE))
+  expect_equal(as.numeric(ncvar_get(nc, nc$var$Y_COORD)), as.numeric(dataFrame$Y_COORD))
+  expect_equal(ncatt_get(nc,0,"Conventions")$value,"CF-1.7")
+  expect_equal(ncatt_get(nc,0,"featureType")$value,"point")
+  expect_equal(ncatt_get(nc,0,"cdm_data_type")$value,"Station")
+  })
+
