1b1448af1970d3921bb439ae284197f6445f2d2d
David Blodgett <dblodgett@usgs.gov>
2019-03-25 19:18:06 -0500

    point dsg to RNetCDF

--- tests/testthat/test_point.R
+++ tests/testthat/test_point.R
@@ -100,7 +100,7 @@ test_that("shapefile_point", {
 test_that("Point data can be written", {
   dataFrame <- read.csv(system.file("extdata/yahara_alb_attributes.csv", package = "ncdfgeom"))
   nc_file <- write_point_dsg(nc_file=tempfile(), lats = dataFrame$YCOORD, lons = dataFrame$XCOORD, 
-                             alts = rep(0, length(dataFrame$XCOORD)), times = as.POSIXct("1970-01-01 00:00:00 GMT"),
+                             alts = rep(0, length(dataFrame$XCOORD)), times = as.POSIXct("1970-01-01 00:00:00 UTC", tz = "UTC"),
                              feature_names = dataFrame$ID,
                              data = dataFrame[c("GRIDCODE", "X_COORD", "Y_COORD")],
                              data_units = c("unitless", "m", "m"), force_v4 = TRUE)
