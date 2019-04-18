aabef776f051a18e286cea2c84c42c5f502b9787
David Blodgett <dblodgett@usgs.gov>
2019-04-18 13:43:31 -0500

    fixes #56

--- R/write_timeseries_dsg.R
+++ R/write_timeseries_dsg.R
@@ -17,6 +17,7 @@
 #' @param data_metadata \code{list} A named list of strings: list(name='ShortVarName', long_name='A Long Name')
 #' @param attributes list An optional list of attributes that will be added at the global level. 
 #' See details for useful attributes.
+#' @param time_units \code{character} units string in udunits format to use for time. Defaults to 'days since 1970-01-01 00:00:00'
 #' @param add_to_existing \code{boolean} If TRUE and the file already exists, 
 #' variables will be added to the existing file. See details for more.
 #' @param overwrite boolean error if file exists.
@@ -58,6 +59,7 @@
 #' @export
 write_timeseries_dsg = function(nc_file, instance_names, lats, lons, times, data, alts=NA, data_unit='',
 																data_prec='double',data_metadata=list(name='data',long_name='unnamed data'),
+																time_units = 'days since 1970-01-01 00:00:00',
 																attributes=list(), add_to_existing=FALSE, overwrite = FALSE){
 	
   if(!overwrite && !add_to_existing && file.exists(nc_file)) stop("File already exists and overwrite is false.")
@@ -143,7 +145,7 @@ write_timeseries_dsg = function(nc_file, instance_names, lats, lons, times, data
 		        data = instance_names)
 		
 		add_var(nc, pkg.env$time_var_name, pkg.env$time_dim_name, "NC_DOUBLE", 
-		        'days since 1970-01-01 00:00:00', -999, 'time of measurement')
+		        time_units, -999, 'time of measurement')
 		
 		add_var(nc, pkg.env$lat_coord_var_name, pkg.env$instance_dim_name, "NC_DOUBLE", 
 		        'degrees_north', -999, 'latitude of the observation')
@@ -190,7 +192,7 @@ write_timeseries_dsg = function(nc_file, instance_names, lats, lons, times, data
 		}
 		
 		#Put data in NC file
-		var.put.nc(nc, pkg.env$time_var_name, as.numeric(times)/86400) #convert to days since 1970-01-01
+		var.put.nc(nc, pkg.env$time_var_name, RNetCDF::utinvcal.nc(time_units, times))
 		var.put.nc(nc, pkg.env$lat_coord_var_name, lats)
 		var.put.nc(nc, pkg.env$lon_coord_var_name, lons)
 		
